14dc05d37457a442f1e4da943676402996e81b6a
Thomas <34007164+kai7win@users.noreply.github.com>
2023-07-12 20:08:39 +0800

    Initial Commit

---
+++ DiceSceneKitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceSceneKitTest
+//
+//  Created by Kai Chi Tsao on 2023/7/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
