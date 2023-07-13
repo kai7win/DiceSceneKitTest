924686eb4bc5e9b0937fc0dd1b90f81bad5f0cff
Thomas <34007164+kai7win@users.noreply.github.com>
2023-07-13 14:08:50 +0800

    物理碰撞效果

--- DiceSceneKitTest/ViewController.swift
+++ DiceSceneKitTest/ViewController.swift
@@ -27,6 +27,11 @@ class ViewController: UIViewController {
         cameraNode.position = SCNVector3(x: 0, y: -1.5, z: 12)
         scene.rootNode.addChildNode(cameraNode)
         
+        
+        let boundingBoxNode = createBoundingBox()
+        scene.rootNode.addChildNode(boundingBoxNode)
+        
+        
         for i in 0...1 {
             var row: [SCNNode] = []
             for j in -1...1 {
@@ -48,6 +53,7 @@ class ViewController: UIViewController {
     
     func createDiceNode() -> SCNNode {
         let diceNode = SCNNode()
+       
         let diceGeometry = SCNBox(width: 0.7, height: 0.7, length: 0.7, chamferRadius: 0.15)
         
         let material1 = SCNMaterial()
@@ -70,44 +76,92 @@ class ViewController: UIViewController {
         
         diceGeometry.materials = [material1, material2, material6, material5, material3, material4]
         diceNode.geometry = diceGeometry
+        
+        
         return diceNode
     }
     
     @objc func rollDice() {
         for (rowIndex, diceRow) in diceNodes.enumerated() {
             for (colIndex, diceNode) in diceRow.enumerated() {
-                // 旋转事件的Array
-                var rotationActions: [SCNAction] = []
                 
-                // 添加5次乱数不同角度的旋转事件 存进Array
-                for _ in 1...5 {
-                    let rotationAction = SCNAction.rotateBy(
-                        x: CGFloat.random(in: 0...CGFloat.pi * 2),
-                        y: CGFloat.random(in: 0...CGFloat.pi * 2),
-                        z: CGFloat.random(in: 0...CGFloat.pi * 2),
-                        duration: 0.5
-                    )
-                    rotationActions.append(rotationAction)
-                }
+                // 记录原始位置
+                let originalPosition = diceNode.position
+                
+                // 创建物理体身体
+                let dicePhysicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(geometry: diceNode.geometry!, options: nil))
+                
+                // 调整物理体的质量和弹性
+                dicePhysicsBody.mass = 0.1 // 根据需要调整这个值
+                dicePhysicsBody.restitution = 0.01 // 降低反弹能量
+                dicePhysicsBody.friction = 0.7 // 增加摩擦力
+
+                diceNode.physicsBody = dicePhysicsBody
+                
+                // 添加力和扭矩
+                let forceMagnitude: Float = 0.2 // 根据需要调整这个值
+                
+                let force = SCNVector3(x: Float.random(in: -forceMagnitude...forceMagnitude),
+                                       y: Float.random(in: -forceMagnitude...forceMagnitude),
+                                       z: Float.random(in: -forceMagnitude...forceMagnitude))
+                diceNode.physicsBody?.applyForce(force, asImpulse: true)
+                
+                let torque = SCNVector4(x: Float.random(in: -1...1), y: Float.random(in: -1...1), z: Float.random(in: -1...1), w: Float.random(in: -1...1))
+                diceNode.physicsBody?.applyTorque(torque, asImpulse: false)
+                
                 
                 // 计算每个骰子的结果Index取出完成骰子需对应的点数
                 let resultIndex = rowIndex * diceRow.count + colIndex
                 let diceFace = self.preSetRollResults[resultIndex % preSetRollResults.count]
                 
+                // 骰子聚集的位置
+                let gatheringPoint = SCNVector3(0, 0.5, 0)
+                
+                // 创建骰子聚集的动作
+                let moveToGatheringPointAction = SCNAction.move(to: gatheringPoint, duration: 0.2)
+                
+//                // 稍微等待一下
+//                let waitAction = SCNAction.wait(duration: 0.2)
+                
+                var rotationActions: [SCNAction] = []
+                // 添加3次乱数不同角度的旋转事件 存进Array
+                for _ in 1...3 {
+                    let rotationAction = SCNAction.rotateBy(
+                        x: CGFloat.random(in: 0...CGFloat.pi ),
+                        y: CGFloat.random(in: 0...CGFloat.pi ),
+                        z: CGFloat.random(in: 0...CGFloat.pi ),
+                        duration: 0.5
+                    )
+                    rotationActions.append(rotationAction)
+                }
                 
                 let completionAction = SCNAction.run { _ in
                     diceNode.eulerAngles = self.rotationForDiceFace(diceFace)
                 }
                 
-                // 添加完成的旋转事件
-                rotationActions.append(completionAction)
+                let makeStaticAction = SCNAction.run { _ in
+                    diceNode.physicsBody?.type = .static
+                }
                 
-                // 执行旋转的动作
-                let sequenceAction = SCNAction.sequence(rotationActions)
+                let endPositionAction = SCNAction.move(to: originalPosition, duration: 0.3)
+                
+                var allActions: [SCNAction] = []
+                
+                /// 移动到一起、碰撞、旋转、完成、静止、回到原本位置
+                allActions.append(contentsOf: [moveToGatheringPointAction])
+                allActions.append(contentsOf: rotationActions)
+                allActions.append(contentsOf: [completionAction,makeStaticAction, endPositionAction])
+                
+                // 执行全部的动作
+                let sequenceAction = SCNAction.sequence(allActions)
                 diceNode.runAction(sequenceAction)
+
+               
+               
             }
         }
     }
+
     
     ///SCNVector3(0, 0, 0) 1
     ///SCNVector3(0, -CGFloat.pi / 2, 0) 2
@@ -115,7 +169,6 @@ class ViewController: UIViewController {
     ///SCNVector3(-CGFloat.pi / 2, 0, 0) 4
     ///SCNVector3(0, CGFloat.pi / 2, 0) 5
     ///SCNVector3(CGFloat.pi, 0, 0) 6
-    
     
     func rotationForDiceFace(_ face: Int) -> SCNVector3 {
         switch face {
@@ -128,6 +181,30 @@ class ViewController: UIViewController {
         default: return SCNVector3(CGFloat.pi, 0, 0)
         }
     }
+    
+    func createBoundingBox() -> SCNNode {
+        let boxGeometry = SCNBox(width: 3, height: 3.0, length: 10.0, chamferRadius: 0)
+        let boxNode = SCNNode(geometry: boxGeometry)
+        
+        // 使几何体看起来是透明的
+        let transparentMaterial = SCNMaterial()
+        transparentMaterial.diffuse.contents = UIColor.clear
+        boxGeometry.materials = [transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial]
+        
+        // 添加物理体
+        let boxPhysicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(geometry: boxNode.geometry!, options: nil))
+        // 调整物理体的质量和弹性
+        boxPhysicsBody.mass = 0.5 // 根据需要调整这个值
+        boxPhysicsBody.restitution = 0.1 // 降低反弹能量
+        boxPhysicsBody.friction = 0.7 // 增加摩擦力
+        boxNode.physicsBody = boxPhysicsBody
+        
+        
+        
+        return boxNode
+    }
+
+    
 }
 
 
